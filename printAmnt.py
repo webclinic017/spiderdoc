733c452d6284a4f6a2774faccf7b90708466110f
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-11-12 23:55:24 +0200

    added some more super basic functions

--- printAmnt.py
+++ printAmnt.py
@@ -11,3 +11,16 @@ if account.trading_blocked:
 
 # Check how much money we can use to open new positions.
 print('${} is available as buying power.'.format(account.buying_power))
+
+# Get a list of all active assets.
+print("active assets:")
+active_assets = api.list_assets(status='active')
+
+# Filter the assets down to just those on NASDAQ.
+nasdaq_assets = [a for a in active_assets if a.exchange == 'NASDAQ']
+print(nasdaq_assets)
+
+# Check if AAPL is tradable on the Alpaca platform.
+aapl_asset = api.get_asset('AAPL')
+if aapl_asset.tradable:
+    print('We can trade AAPL.')
