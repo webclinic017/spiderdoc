a68249b134632f949f915687a6cbb72aaf31b3b4
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-11-13 15:18:24 +0200

    test historical data on linux t:001

--- printAmnt.py
+++ printAmnt.py
@@ -1,4 +1,8 @@
 import alpaca_trade_api as tradeapi
+import ta
+import pandas as pd
+import matplotlib as plt
+import yfinance as yf
 
 api = tradeapi.REST('PK0GXVSBKQ0WO0B1CEUQ','CpcoieCzUchnKnRKWjjqLuVzGc5ECqrdvJGNnJrE','https://paper-api.alpaca.markets',)
 
@@ -11,17 +15,7 @@ if account.trading_blocked:
 
 # Check how much money we can use to open new positions.
 print('${} is available as buying power.'.format(account.buying_power))
-
-# Get daily price data for AAPL over the last 5 trading days.
-barset = api.get_barset('AAPL', 'day', limit=5)
-aapl_bars = barset['AAPL']
-
-# Get daily price data for AAPL over the last 5 trading days.
-barset = api.get_barset('AAPL', 'day', limit=5)
-aapl_bars = barset['AAPL']
-
-# See how much AAPL moved in that timeframe.
-week_open = aapl_bars[0].o
-week_close = aapl_bars[-1].c
-percent_change = (week_close - week_open) / week_open * 100
-print('AAPL moved {}% over the last 5 days'.format(percent_change))
+stock_to_trade = "AAPL"
+curr_stock = yf.Ticker(stock_to_trade)
+curr_stock_historical = curr_stock.history(start="2021-11-02", end="2021-11-03", interval="1m")
+print(curr_stock_historical)
