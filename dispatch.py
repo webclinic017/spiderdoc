c35c50108e7a71c091458df98e36b33adfcf01b6
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-11-23 19:39:53 +0200

    ssh sp

--- dispatch.py
+++ dispatch.py
@@ -1,7 +1,22 @@
 import os
 import paramiko
+import sys
+import subprocess
 from fsplit.filesplit import Filesplit
-
+################################################################################################
+                            #functions
+def cmd_over_ssh (hosname,cmd):
+    ssh = subprocess.Popen(["ssh", "%s" % hosname, cmd],
+                       shell=False,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+    result = ssh.stdout.readlines()
+    if result == []:
+        error = ssh.stderr.readlines()
+        print >>sys.stderr, "ERROR: %s" % error
+    else:
+        return result                         
+################################################################################################
 fs = Filesplit()
 active_hosts=0
 hostnames = ["BT-1","BT-2","BT-3","BT-4"]
@@ -26,17 +41,14 @@ end_date = str('2021-11-12')
 #run computefor with args:start-date end-date Sym file suffix.
 
 #use RSA private key to connect
-k = paramiko.RSAKey.from_private_key_file("/home/ubuntu/.ssh/id_rsa")
-ssh = paramiko.SSHClient()
 i=1
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname=host, username='ubuntu', pkey=k)
 for host in active_hosts:
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('docker build -t algo1d:1.0.0 /appcode/spiderdoc')
+    res=cmd_over_ssh(host,'mkdir lookatme')
     print("GOT 1!")
-for host in active_hosts:
+""" for host in active_hosts:
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname=host, username='ubuntu', pkey=k)
     print("got here 2")
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('/appcode/spiderdoc/backtest /appcode/input/tmp/symbols/Symbols_'+str(i)+" "+start_date+" "+end_date)
     i += 1
+ """
