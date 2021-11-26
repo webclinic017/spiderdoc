e6b851043f37f6b85a175f1345eddc878fbf8888
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-11-26 17:04:17 +0200

    -

--- BT/SMA.py
+++ BT/SMA.py
@@ -171,7 +171,7 @@ for index, row in curr_stock_historical['Close'].iteritems():
 
 
 outname = 'SMA-'+stock_to_trade+'-X-'+start_date+'.csv'
-outdir = '/outfile/position'
+outdir = '/outfile/position/'
 #outdir = 'C:\DEVOPS\python apps\spiderdoc\spiderdoc\outfile\positions\''
 fullname =  outdir + outname
 positions.to_csv(fullname)
