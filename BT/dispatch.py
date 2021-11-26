d62a1ac67dfe70a7c595b7bca9216287b34b72a4
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-11-26 16:01:19 +0200

    -

--- BT/dispatch.py
+++ BT/dispatch.py
@@ -1,12 +1,13 @@
 import os
-import asyncio
 import sys
 import subprocess
 from fsplit.filesplit import Filesplit
-
 import multiprocessing.process
+import numpy as np
+
 ################################################################################################
                             #functions
+#this function starts a sub proc to run a cmd via ssh                           
 def cmd_over_ssh (hosname,cmd):
     ssh = subprocess.Popen(["ssh", "%s" % hosname, cmd],
                        shell=False,
@@ -20,11 +21,16 @@ def cmd_over_ssh (hosname,cmd):
         return result
                     
 ################################################################################################
+#var definitions
 fs = Filesplit()
 active_hosts=0
 hostnames = ["BT-1","BT-2","BT-3","BT-4"]
 active_hosts = []
 Pros=[]
+start_date = input('enter start date in YYYY-mm-dd format : ')
+end_date = input('enter end date in YYYY-mm-dd format : ')
+print('FLT - no balance ,constatnt stock amount | ADJ - adjusts balance and stock amount each trade | REAL - Saves balance next day ')
+run_type = input('enter run type(FLT|ADJ|REAL) : ')
 #see how many servers are up
 for host in hostnames:
     response = os.system("ping -c 1 " + host)
@@ -33,9 +39,10 @@ for host in hostnames:
         print(host+' <===> is Online')
     else:
          pingstatus = "Network Error"
+
 i=len(active_hosts)
 #n servers are up . create from "Symbols" n files : Sym1,Sym2..Symn
-size_per_file = int((os.stat('Symbols').st_size) / i)+1
+size_per_file = int((os.stat('Symbols').st_size) / i)+256
 smallfile = None
 filenum=1
 fs.split(file="/appcode/spiderdoc/Symbols", split_size=size_per_file, output_dir="/appcode/input/tmp/symbols", newline=True,)
@@ -44,12 +51,12 @@ start_date = str('2021-11-02')
 end_date = str('2021-11-12')
 i=1
 for host in active_hosts:
-    res= cmd_over_ssh(host,'docker build -t backtest:1.0.0 /appcode/spiderdoc')
+    res= cmd_over_ssh(host,'docker build -t backtest:1.0.0 /appcode/spiderdoc/BT')
 
     print("GOT 1!")
 
 for host in active_hosts:
-    p = multiprocessing.Process(target=cmd_over_ssh, args=(host,'/appcode/spiderdoc/backtest Symbols_'+str(i)+' '+start_date+' '+end_date))
+    p = multiprocessing.Process(target=cmd_over_ssh, args=(host,'/appcode/spiderdoc/BT/backtest Symbols_'+str(i)+' '+start_date+' '+end_date))
     Pros.append(p)
     p.start()
     print("started backtest on :" + host)
