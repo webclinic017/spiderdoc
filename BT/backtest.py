2583d4a7c51b3bfdbbae656a8ad4d4dae5081f45
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-11-27 18:24:40 +0200

    -

--- BT/backtest.py
+++ BT/backtest.py
@@ -1,35 +1,37 @@
 import subprocess
 import sys
 import multiprocessing
-from typing import Container
-""" 
+
 file = sys.argv[1]
 start_date_range = sys.argv[2]
 end_date_range = sys.argv[3]
-run_type = sys.argv[4]       """
+run_type = sys.argv[4]  
 
-def run_command(line):
-    global start_date_range
-    global end_date_range
-    global run_type 
-    #command = "docker run -v /appcode/output/positions:/outfile/position backtest:1.0.0 "+line+' '+start_date_range+' '+ end_date_range+' '+ run_type
-    #subprocess.Popen(command, shell=True)
-    print(line+' '+start_date_range+' '+end_date_range+' '+run_type)
-
-file = 'Symbols_1'
+""" file = 'Symbols_1'
 start_date_range = '2021-11-02'
 end_date_range = '2021-11-03'
-run_type = 'ADJ' 
-
+run_type = 'ADJ'  """
 pros = []
-#file='/appcode/input/tmp/symbols/'+file
-file='BT\Symbols'
-container_amnt = 8
-lines =[]
+file='/appcode/input/tmp/symbols/'+file
 f = open(file, "r")
 while True:
-    for line_num in range(container_amnt) :
-        line = f.readline()
-        lines.append(line.rstrip('\n'))
-    pool = multiprocessing.Pool(container_amnt)
-    pool.starmap(run_command,lines)
+    line1 = f.readline()
+    if not line1: break
+    p = multiprocessing.Process(subprocess.Popen(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line1.rstrip('\n'),start_date_range,end_date_range,run_type]))
+    pros.append(p)
+    p.start()
+    line2 = f.readline()
+    if not line2: break
+    p = multiprocessing.Process(subprocess.Popen(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line2.rstrip('\n'),start_date_range,end_date_range,run_type]))
+    pros.append(p)
+    p.start()
+    line3 = f.readline()
+    if not line3: break
+    p = multiprocessing.Process(subprocess.Popen(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line3.rstrip('\n'),start_date_range,end_date_range,run_type]))
+    pros.append(p)
+    p.start()
+    line4 = f.readline()
+    if not line4: break
+    p = multiprocessing.Process(subprocess.Popen(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line4.rstrip('\n'),start_date_range,end_date_range,run_type]))
+    pros.append(p)
+    p.start()
