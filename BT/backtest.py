bb0fdab673fa8e0c7447d6a9e24087cdc05af532
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-11-27 03:38:38 +0200

    -

--- BT/backtest.py
+++ BT/backtest.py
@@ -16,15 +16,15 @@ f = open(file, "r")
 while True:
     line1 = f.readline()
     if not line1: break
-    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line1,start_date_range,end_date_range,run_type])
+    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line1.rstrip('\n'),start_date_range,end_date_range,run_type])
     line2 = f.readline()
     if not line2: break
-    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line2,start_date_range,end_date_range,run_type])
+    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line2.rstrip('\n'),start_date_range,end_date_range,run_type])
     line3 = f.readline()
     if not line3: break
-    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line3,start_date_range,end_date_range,run_type])
+    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line3.rstrip('\n'),start_date_range,end_date_range,run_type])
     line4 = f.readline()
     if not line4: break
-    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line4,start_date_range,end_date_range,run_type])
+    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line4.rstrip('\n'),start_date_range,end_date_range,run_type])
 
     
