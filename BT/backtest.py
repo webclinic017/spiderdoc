ba44a75b0c0a3aae941dc053532f0521effc1212
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-11-27 02:20:00 +0200

    -

--- BT/backtest.py
+++ BT/backtest.py
@@ -1,5 +1,7 @@
 import subprocess
 import sys
+import docker
+import os
 
 file = sys.argv[1]
 start_date_range = sys.argv[2]
@@ -16,15 +18,19 @@ f = open(file, "r")
 while True:
     line1 = f.readline()
     if not line1: break
-    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line1,start_date_range,end_date_range,run_type])
+    #subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line1,start_date_range,end_date_range,run_type])
+    os.system(" docker run -v /appcode/output/positions:/outfile/position backtest:1.0.0 "+line1+' '+start_date_range+' '+end_date_range+' '+run_type)
     line2 = f.readline()
     if not line2: break
-    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line2,start_date_range,end_date_range,run_type])
+    #subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line2,start_date_range,end_date_range,run_type])
+    os.system(" docker run -v /appcode/output/positions:/outfile/position backtest:1.0.0 "+line2+' '+start_date_range+' '+end_date_range+' '+run_type)
     line3 = f.readline()
     if not line3: break
-    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line3,start_date_range,end_date_range,run_type])
+    #subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line3,start_date_range,end_date_range,run_type])
+    os.system(" docker run -v /appcode/output/positions:/outfile/position backtest:1.0.0 "+line3+' '+start_date_range+' '+end_date_range+' '+run_type)
     line4 = f.readline()
     if not line4: break
-    subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line4,start_date_range,end_date_range,run_type])
+    #subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line4,start_date_range,end_date_range,run_type])
+    os.system(" docker run -v /appcode/output/positions:/outfile/position backtest:1.0.0 "+line4+' '+start_date_range+' '+end_date_range+' '+run_type)
 
     
