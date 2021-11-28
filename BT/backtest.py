629d66dccb2f8b04204b383ff2bf7cbcc199dcea
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-11-28 14:53:43 +0200

    only n containers are up anytime, pool.starmap

--- BT/backtest.py
+++ BT/backtest.py
@@ -1,32 +1,35 @@
 import subprocess
 import sys
 import multiprocessing
-from multiprocessing import Pool
-def start_container (line,start_date_range,end_date_range,run_type):
+from typing import Container
+def start_container (line):
+    global start_date_range
+    global end_date_range
+    global run_type
     subprocess.call(["docker", "run", "-v","/appcode/output/positions:/outfile/position", "backtest:1.0.0", line,start_date_range,end_date_range,run_type])
 
-file = sys.argv[1]
+""" file = sys.argv[1]
 start_date_range = sys.argv[2]
 end_date_range = sys.argv[3]
-run_type = sys.argv[4]   
+run_type = sys.argv[4]   """
 
-""" file = 'Symbols_1'
+file = 'Symbols_1'
 start_date_range = '2021-11-02'
 end_date_range = '2021-11-20'
-run_type = 'ADJ'  """
+run_type = 'ADJ' 
 pros = []
 lines = []
-file='/appcode/input/tmp/symbols/'+file
+file='/home/ubuntu/testsym'
 f = open(file, "r")
-container_amnt=4
+container_amnt = 4
 while True:
     for line_num in range(container_amnt):
         pros=[]
         lines.append(f.readline())
         if __name__ == '__main__':
             for line in lines:
-                with multiprocessing.Pool(processes=3) as pool:
-                    results = pool.starmap(target=start_container,args=[line,start_date_range,end_date_range,run_type])
+                with multiprocessing.Pool(processes=container_amnt) as pool:
+                    pool.starmap(start_container,line)
         if len(lines) < container_amnt:
             break
         lines=[]
