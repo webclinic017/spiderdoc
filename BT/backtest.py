6b658a0a7af55dedab6594583408900752bc7e79
ubuntu <ariel.tar99@gmail.com>
2021-11-30 23:42:21 +0000

    try to run with starmap

--- BT/backtest.py
+++ BT/backtest.py
@@ -8,9 +8,11 @@ def start_container (symbols_file):
     global start_date_range
     global end_date_range
     global run_type
-
-    subprocess.call(["docker", "run", "-v","/containers/output:/output", "backtest:1.0.0", symbols_file,start_date_range,end_date_range,run_type])
-
+    global containers_up
+    subprocess.Popen(["docker", "run", "-v","/containers/output:/output", "backtest:1.0.0", symbols_file,start_date_range,end_date_range,run_type])
+    containers_up += 1
+    if containers_up >= 4 :
+        exit
 file = sys.argv[1]
 start_date_range = sys.argv[2]
 end_date_range = sys.argv[3]
@@ -35,7 +37,7 @@ for sufx in range(1,container_amnt+1):
     sym_list.append(file+'_'+str(sufx))
 print(sym_list)    
 pool = multiprocessing.Pool(processes=container_amnt)
-if __name__ == '__main__':
-    while True:
-        pool.map_async(start_container,sym_list) 
-
+containers_up=0
+if __name__ == '__main__' :
+    while True :
+        pool.starmap_async(start_container,sym_list) 
