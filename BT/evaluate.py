1291fdfbddb80ddb19e752ed85d66856b34e3842
Denis <den.stre9@gmail.com>
2021-12-01 21:18:54 +0200

    fixed reading - removed unecessary rows

--- BT/evaluate.py
+++ BT/evaluate.py
@@ -6,28 +6,42 @@ from datetime import datetime,timedelta,time
 import matplotlib.pyplot as plt 
 
 delta=pd.DataFrame(columns=['Value'])
-#get all data from csv into a single df
-path = 'C:\\DEVOPS\\python apps\\spiderdoc\\spiderdoc\\automation\\outfile\\positions\\'
+
+#get all data from csv into a single df - 0 rows deleted - open positions deleted
+path = 'C:\\Users\\Denis\\Desktop\\Project Money Printer\\money printer\\positions\\' # CHANGE PATH TO RELEVANT PATH FOR PROJECT
 all_files = glob.glob(path + "*.csv")
 print(all_files)
 print(path)
 li = []
 
 for filename in all_files:
-    positions = pd.read_csv(filename, index_col=[0])
-    li.append(positions)
+    positions = pd.read_csv(filename)  #read single file
 
-positions = pd.concat(li, axis=0, ignore_index=True)
-print(positions)
+    positions.drop(index=positions.index[0],axis=0, inplace=True)  #drop 0 rows
+
+    if(not positions.empty):
+        if(positions.iloc[-1]['Intent'] == "SHORT" or positions.iloc[-1]['Intent'] == "LONG") :  #drop open positions
+           positions.drop(index=positions.index[-1],axis=0, inplace=True)
+
+    li.append(positions)  #add file to list
+   
+
+positions = pd.concat(li, axis=0, ignore_index=True)  #combine all files to single df
+
+#######end
+    
+#gross profit
 total = 0
-delta_index= 0 
 for index, row in islice(positions.iterrows(), 0, None):
     if positions.loc[index]["Action"] == "buy" :
         total = total - positions.loc[index]["TValue"]
     else:
         total = total + positions.loc[index]["TValue"]
 print(total)
+#end
+
 #for eval
+delta_index= 0 
 for index, row in islice(positions.iterrows(), 0, None):
     if positions.loc[index]["Intent"] == "CLOSE_LONG" :
         delta.loc[delta_index] = positions.loc[index]['TValue'] - positions.loc[index-1]['TValue']
@@ -36,6 +50,9 @@ for index, row in islice(positions.iterrows(), 0, None):
         delta.loc[delta_index] =positions.loc[index-1]['TValue'] - positions.loc[index]['TValue'] 
         delta_index += 1
 
-delta.plot.bar(y='Value', rot=0)
+
+delta.reset_index().plot.scatter(x = 'index', y = 'Value')
+plt.grid(True)
 plt.show()
 print(delta)        
+ 
