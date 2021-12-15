a44d81c3ab0251ca05f5ba79717562c03b6dcca2
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-12-15 21:05:09 +0200

    all necesary modules updated

--- BT/containers/srfinder/local_min_max.py
+++ BT/containers/srfinder/local_min_max.py
@@ -1,22 +1,141 @@
-from numpy.lib.function_base import append
 import yfinance as yf
 import pandas as pd
-import matplotlib
 import ta
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mpl_dates
 from scipy.signal import argrelextrema
 from sklearn.cluster import KMeans
+from datetime import timedelta,time,datetime
+import sys
+import multiprocessing
 
+get_rid_of_position = False
+position_is_open=False
+""" symbols_file = sys.argv[1]
+start_date_range = sys.argv[2]
+end_date_range = sys.argv[3]
+run_type = sys.argv[4]
+prallel_proc_amnt = sys.argv[5]
+prallel_proc_amnt=int(prallel_proc_amnt) """
 
+positions              = pd.DataFrame(columns=['Timestamp','Action','Amount','Price','TValue','Intent','Balance'])
+#for eval 
+positions_short        = pd.DataFrame(columns=['Action','Price','Amount','TValue','Intent'])
+positions_closed_short = pd.DataFrame(columns=['Action','Price','Amount','TValue','Intent'])
+positions_long         = pd.DataFrame(columns=['Action','Price','Amount','TValue','Intent'])
+positions_closed_longs = pd.DataFrame(columns=['Action','Price','Amount','TValue','Intent'])
+positions.set_index('Timestamp')
+#assign before referance
+########################################################################################################################
+#                                           FUNCTIONS
+########################################################################################################################
+
+def update_pos(index,action,price,amount,tvalue,intent,balance) :
+    global positions
+    positions.loc[index,'Action']=action
+    positions.loc[index,'Amount']=amount
+    positions.loc[index,'Price']=price
+    positions.loc[index,'TValue']=tvalue
+    positions.loc[index,'Intent']=intent
+    positions.loc[index,'Balance']=balance
+
+def update_pos_long(index,action,amount,price,tvalue,intent) :
+    positions_long .loc[index,'Action']=action
+    positions_long.loc[index,'Amount']=amount
+    positions_long.loc[index,'Price']=price
+    positions_long.loc[index,'TValue']=tvalue
+    positions_long.loc[index,'Intent']=intent
+    
+def update_pos_closed_long(index,action,amount,price,tvalue,intent) :
+    positions_closed_longs.loc[index,'Action']=action
+    positions_closed_longs.loc[index,'Amount']=amount
+    positions_closed_longs.loc[index,'Price']=price
+    positions_closed_longs.loc[index,'TValue']=tvalue
+    positions_closed_longs.loc[index,'Intent']=intent
+    
+def update_pos_short(index,action,amount,price,tvalue,intent) :
+    positions.loc[index,'Action']=action
+    positions_short.loc[index,'Amount']=amount
+    positions_short.loc[index,'Price']=price
+    positions_short.loc[index,'TValue']=tvalue
+    positions_short.loc[index,'Intent']=intent
+    
+def update_pos_closed_short(index,action,amount,price,tvalue,intent) :
+    positions_closed_short.loc[index,'Action']=action
+    positions_closed_short.loc[index,'Amount']=amount
+    positions_closed_short.loc[index,'Price']=price
+    positions_closed_short.loc[index,'TValue']=tvalue
+    positions_closed_short.loc[index,'Intent']=intent
+
+def update_balance (balance,trans_value,action,intent):  
+    if (action == 'buy') :
+        balance = balance - trans_value
+    else:
+        balance = balance + trans_value
+    return balance
+def update_stock_amnt (balance,stock_price,action):
+    tmp_mod=balance % stock_price
+    tmp_balace=balance-tmp_mod
+    stock_amnt=tmp_balace / stock_price
+
+    return stock_amnt
 
 def k_clusters(k,df):
     cluster_list = []
     kmeans = KMeans(n_clusters=k).fit(df.array.reshape(-1,1))
     centroids = kmeans.cluster_centers_
     return centroids  
-def run_sim():
+def run_simulation(stock_to_trade):
+    get_rid_of_position = False
+    position_is_open=False
+    stock_not_avail = False
+    global start_date_range 
+    global end_date_range 
+    global run_type 
+    stock_to_trade=stock_to_trade.strip('\n')
+   
+    a = datetime.strptime(start_date_range, "%Y-%m-%d")
+    b = datetime.strptime(end_date_range, "%Y-%m-%d")
+    delta_date = b - a
+    #initialize position dataframe with null's ans 0's
+    curr_date = start_date_range
+    curr_date = datetime. strptime(curr_date, '%Y-%m-%d')
+    curr_date=curr_date - timedelta(days=1) 
+    balance = 10000
+
+    for day in range(delta_date.days):
+    #########################################################################################################################
+    #                                           New Day initilazion
+    #                                           ==================
+    #########################################################################################################################                                            
+        global positions
+        positions              = pd.DataFrame(columns=['Timestamp','Action','Amount','Price','TValue','Intent'])
+        #for eval 
+        positions_short        = pd.DataFrame(columns=['Action','Price','Amount','TValue','Intent'])
+        positions_closed_short = pd.DataFrame(columns=['Action','Price','Amount','TValue','Intent'])
+        positions_long         = pd.DataFrame(columns=['Action','Price','Amount','TValue','Intent'])
+        positions_closed_longs = pd.DataFrame(columns=['Action','Price','Amount','TValue','Intent'])
+        curr_date=curr_date + timedelta(days=1) 
+        update_pos('0000-00-00 00:00:00-00:00','NA',0,0,0,'NA',balance)
+        if run_type != 'REAL' :
+            balance=10000
+        tommorow_date =curr_date + timedelta(days=1)
+        get_rid_of_position = False 
+        #skips loop run if staurday
+        if curr_date.weekday() == 5 or curr_date.weekday() == 6 :
+            continue
+        ########################################################################################################################
+        #                                      Data and Metrics for this day are calculated Here
+        #                                     ===================================================
+        ########################################################################################################################
+        #get historical data from yfinance for this day
+        try:
+            curr_stock_historical = yf.download(stock_to_trade,curr_date,tommorow_date,interval='1m')
+            curr_stock_historical.head()
+        except:
+            stock_not_avail = True
+            break
     curr_stock_historical = yf.download("TSLA","2021-11-22","2021-11-23",interval='1m')
 
     #create figure
@@ -93,4 +212,18 @@ def run_sim():
 
     #display candlestick chart ,EMA_[wide,med,thin] ,first n local min/max of 1st period of the day,
     plt.show()
+    curr_stock_historical=curr_stock_historical.iloc[60:,:]
     
+stock_to_trade = 'AAPL'
+start_date_range = '2021-12-13'
+end_date_range = '2021-12-14'
+run_type = 'ADJ'
+run_simulation(stock_to_trade)
+""" file_path = '/input/'+symbols_file
+Sym_file = open(file_path,"r")
+
+
+if __name__ == '__main__':
+    # start n worker processes
+    with multiprocessing.Pool(processes=prallel_proc_amnt) as pool:
+        pool.map_async(run_simulation,iterable=Sym_file).get()   """
