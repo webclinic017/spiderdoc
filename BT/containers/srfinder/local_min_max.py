033080b7e40ba9a1bd01f7fc8f73aeb68dd766bb
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2021-12-27 23:59:17 +0200

    patterns are recognized &ranked&sumed if>1 aprears

--- BT/containers/srfinder/local_min_max.py
+++ BT/containers/srfinder/local_min_max.py
@@ -2,6 +2,7 @@ from contextlib import nullcontext
 from re import X
 import re
 from numpy.lib.function_base import append
+from scipy.signal.ltisys import dfreqresp
 import yfinance as yf
 import pandas as pd
 import ta
@@ -15,7 +16,133 @@ from datetime import timedelta,time,datetime
 import sys
 import multiprocessing
 from sklearn.metrics import silhouette_score
-from itertools import islice
+
+from itertools import compress
+
+global candle_rankings
+
+candle_rankings = {
+        "CDL3LINESTRIKE_Bull": 1,
+        "CDL3LINESTRIKE_Bear": 2,
+        "CDL3BLACKCROWS_Bull": 3,
+        "CDL3BLACKCROWS_Bear": 3,
+        "CDLEVENINGSTAR_Bull": 4,
+        "CDLEVENINGSTAR_Bear": 4,
+        "CDLTASUKIGAP_Bull": 5,
+        "CDLTASUKIGAP_Bear": 5,
+        "CDLINVERTEDHAMMER_Bull": 6,
+        "CDLINVERTEDHAMMER_Bear": 6,
+        "CDLMATCHINGLOW_Bull": 7,
+        "CDLMATCHINGLOW_Bear": 7,
+        "CDLABANDONEDBABY_Bull": 8,
+        "CDLABANDONEDBABY_Bear": 8,
+        "CDLBREAKAWAY_Bull": 10,
+        "CDLBREAKAWAY_Bear": 10,
+        "CDLMORNINGSTAR_Bull": 12,
+        "CDLMORNINGSTAR_Bear": 12,
+        "CDLPIERCING_Bull": 13,
+        "CDLPIERCING_Bear": 13,
+        "CDLSTICKSANDWICH_Bull": 14,
+        "CDLSTICKSANDWICH_Bear": 14,
+        "CDLTHRUSTING_Bull": 15,
+        "CDLTHRUSTING_Bear": 15,
+        "CDLINNECK_Bull": 17,
+        "CDLINNECK_Bear": 17,
+        "CDL3INSIDE_Bull": 20,
+        "CDL3INSIDE_Bear": 56,
+        "CDLHOMINGPIGEON_Bull": 21,
+        "CDLHOMINGPIGEON_Bear": 21,
+        "CDLDARKCLOUDCOVER_Bull": 22,
+        "CDLDARKCLOUDCOVER_Bear": 22,
+        "CDLIDENTICAL3CROWS_Bull": 24,
+        "CDLIDENTICAL3CROWS_Bear": 24,
+        "CDLMORNINGDOJISTAR_Bull": 25,
+        "CDLMORNINGDOJISTAR_Bear": 25,
+        "CDLXSIDEGAP3METHODS_Bull": 27,
+        "CDLXSIDEGAP3METHODS_Bear": 26,
+        "CDLTRISTAR_Bull": 28,
+        "CDLTRISTAR_Bear": 76,
+        "CDLGAPSIDESIDEWHITE_Bull": 46,
+        "CDLGAPSIDESIDEWHITE_Bear": 29,
+        "CDLEVENINGDOJISTAR_Bull": 30,
+        "CDLEVENINGDOJISTAR_Bear": 30,
+        "CDL3WHITESOLDIERS_Bull": 32,
+        "CDL3WHITESOLDIERS_Bear": 32,
+        "CDLONNECK_Bull": 33,
+        "CDLONNECK_Bear": 33,
+        "CDL3OUTSIDE_Bull": 34,
+        "CDL3OUTSIDE_Bear": 39,
+        "CDLRICKSHAWMAN_Bull": 35,
+        "CDLRICKSHAWMAN_Bear": 35,
+        "CDLSEPARATINGLINES_Bull": 36,
+        "CDLSEPARATINGLINES_Bear": 40,
+        "CDLLONGLEGGEDDOJI_Bull": 37,
+        "CDLLONGLEGGEDDOJI_Bear": 37,
+        "CDLHARAMI_Bull": 38,
+        "CDLHARAMI_Bear": 72,
+        "CDLLADDERBOTTOM_Bull": 41,
+        "CDLLADDERBOTTOM_Bear": 41,
+        "CDLCLOSINGMARUBOZU_Bull": 70,
+        "CDLCLOSINGMARUBOZU_Bear": 43,
+        "CDLTAKURI_Bull": 47,
+        "CDLTAKURI_Bear": 47,
+        "CDLDOJISTAR_Bull": 49,
+        "CDLDOJISTAR_Bear": 51,
+        "CDLHARAMICROSS_Bull": 50,
+        "CDLHARAMICROSS_Bear": 80,
+        "CDLADVANCEBLOCK_Bull": 54,
+        "CDLADVANCEBLOCK_Bear": 54,
+        "CDLSHOOTINGSTAR_Bull": 55,
+        "CDLSHOOTINGSTAR_Bear": 55,
+        "CDLMARUBOZU_Bull": 71,
+        "CDLMARUBOZU_Bear": 57,
+        "CDLUNIQUE3RIVER_Bull": 60,
+        "CDLUNIQUE3RIVER_Bear": 60,
+        "CDL2CROWS_Bull": 61,
+        "CDL2CROWS_Bear": 61,
+        "CDLBELTHOLD_Bull": 62,
+        "CDLBELTHOLD_Bear": 63,
+        "CDLHAMMER_Bull": 65,
+        "CDLHAMMER_Bear": 65,
+        "CDLHIGHWAVE_Bull": 67,
+        "CDLHIGHWAVE_Bear": 67,
+        "CDLSPINNINGTOP_Bull": 69,
+        "CDLSPINNINGTOP_Bear": 73,
+        "CDLUPSIDEGAP2CROWS_Bull": 74,
+        "CDLUPSIDEGAP2CROWS_Bear": 74,
+        "CDLGRAVESTONEDOJI_Bull": 77,
+        "CDLGRAVESTONEDOJI_Bear": 77,
+        "CDLHIKKAKEMOD_Bull": 82,
+        "CDLHIKKAKEMOD_Bear": 81,
+        "CDLHIKKAKE_Bull": 85,
+        "CDLHIKKAKE_Bear": 83,
+        "CDLENGULFING_Bull": 84,
+        "CDLENGULFING_Bear": 91,
+        "CDLMATHOLD_Bull": 86,
+        "CDLMATHOLD_Bear": 86,
+        "CDLHANGINGMAN_Bull": 87,
+        "CDLHANGINGMAN_Bear": 87,
+        "CDLRISEFALL3METHODS_Bull": 94,
+        "CDLRISEFALL3METHODS_Bear": 89,
+        "CDLKICKING_Bull": 96,
+        "CDLKICKING_Bear": 102,
+        "CDLDRAGONFLYDOJI_Bull": 98,
+        "CDLDRAGONFLYDOJI_Bear": 98,
+        "CDLCONCEALBABYSWALL_Bull": 101,
+        "CDLCONCEALBABYSWALL_Bear": 101,
+        "CDL3STARSINSOUTH_Bull": 103,
+        "CDL3STARSINSOUTH_Bear": 103,
+        "CDLDOJI_Bull": 104,
+        "CDLDOJI_Bear": 104 ,
+        "CDLLONGLINE_Bull": 53,
+        "CDLLONGLINE_Bear": 53,
+        "CDLSHORTLINE_Bull": 85,
+        "CDLSHORTLINE_Bear": 66,
+        "CDLSTALLEDPATTERN_Bull": 93,
+        "CDLSTALLEDPATTERN_Bear": 93,
+        "CDLKICKINGBYLENGTH": 96,
+        "CDLKICKINGBYLENGTH_Bear": 102
+    }
 
 get_rid_of_position = False
 position_is_open=False
@@ -254,18 +381,64 @@ def get_target_price(level,close):
         tp = close + delta_target
     return tp
    
-def is_bull_hammer(df,i):
-    
-    return 0
-def is_bear_hammer():
-    return 0
-def is_bull_engulf():
-    return 0
-def is_bear_engulf():
-    return 0
    
-def patter_indicator():
-#bull patter recognize func call
+def insert_pattern(cdf,i):
+    global curr_stock_historical
+    global candle_rankings
+
+    df = curr_stock_historical.iloc[:i,:]
+# extract OHLC 
+    op = df['Open']
+    hi = df['High']
+    lo = df['Low']
+    cl = df['Close']
+    
+    # create columns for each pattern
+    for candle in candle_names:
+        # below is same as;
+        # df["CDL3LINESTRIKE"] = talib.CDL3LINESTRIKE(op, hi, lo, cl)
+        df[candle] = getattr(talib, candle)(op, hi, lo, cl)
+        
+        #reduce helper cols in to one col
+        
+    df['candlestick_pattern'] = np.nan
+    df['candlestick_match_count'] = np.nan
+    for index, row in df.iterrows():
+
+        # no pattern found
+        if len(row[candle_names]) - sum(row[candle_names] == 0) == 0:
+            df.loc[index,'candlestick_pattern'] = "NO_PATTERN"
+            df.loc[index, 'candlestick_match_count'] = 0
+        # single pattern found
+        elif len(row[candle_names]) - sum(row[candle_names] == 0) == 1:
+            # bull pattern 100 or 200
+            if any(row[candle_names].values > 0):
+                pattern = list(compress(row[candle_names].keys(), row[candle_names].values != 0))[0] + '_Bull'
+                df.loc[index, 'candlestick_pattern'] = pattern
+                df.loc[index, 'candlestick_match_count'] = 1
+            # bear pattern -100 or -200
+            else:
+                pattern = list(compress(row[candle_names].keys(), row[candle_names].values != 0))[0] + '_Bear'
+                df.loc[index, 'candlestick_pattern'] = pattern
+                df.loc[index, 'candlestick_match_count'] = 1
+        # multiple patterns matched -- select best performance
+        else:
+            # filter out pattern names from bool list of values
+            patterns = list(compress(row[candle_names].keys(), row[candle_names].values != 0))
+            container = []
+            for pattern in patterns:
+                if row[pattern] > 0:
+                    container.append(pattern + '_Bull')
+                else:
+                    container.append(pattern + '_Bear')
+            rank_list = [candle_rankings[p] for p in container]
+            if len(rank_list) == len(container):
+                rank_index_best = rank_list.index(min(rank_list))
+                df.loc[index, 'candlestick_pattern'] = container[rank_index_best]
+                df.loc[index, 'candlestick_match_count'] = len(container)
+    # clean up candle columns
+    df.drop(candle_names, axis = 1, inplace = True)
+    return df
 
 #bear patter recognize func call 
     return 0    
@@ -281,6 +454,7 @@ def run_simulation(stock_to_trade):
     global curr_stock_historical_bkp
     global levels
     global balance
+    global candle_names
     stock_to_trade=stock_to_trade.strip('\n')
    
     a = datetime.strptime(start_date_range, "%Y-%m-%d")
@@ -369,9 +543,12 @@ def run_simulation(stock_to_trade):
         curr_stock_historical = curr_stock_historical.loc[:,['Datetime', 'Open', 'High', 'Low', 'Close','ema_thin','ema_med','ema_wide','trend']]
         s =  np.mean(curr_stock_historical['High'] - curr_stock_historical['Low'])    
         
-        #for patter recognition        
+        #for patter recognition (global to feach it ones only)    
         candle_names = talib.get_function_groups()['Pattern Recognition']
-        print(candle_names)
+        
+        candle_df = pd.DataFrame(columns=["Datetime"])
+        candle_df =candle_df.loc[:,['Datetime']]
+        
         ##########################################################################################################################
         #                                       RUN THROUGH DAY                                                                  #   
         ##########################################################################################################################
@@ -389,7 +566,11 @@ def run_simulation(stock_to_trade):
             close=curr_stock_historical['Close'][i]
             #current identified trend
             trend = curr_stock_historical['trend'][i]
-
+            
+            #fill candle pattern overtime
+            candle_df = insert_pattern(candle_df,i)
+            
+            
             #search for snr live
             if isSupport(curr_stock_historical,i):
                 l = curr_stock_historical['Low'][i]
@@ -399,7 +580,6 @@ def run_simulation(stock_to_trade):
                 l = curr_stock_historical['High'][i]
                 if isFarFromLevel(l,levels,s):
                     levels.append((real_index,l))
-            pir=patter_indicator()
             sup = get_support(i,close)
             print('S : '+ str(sup))
             res= get_resistance(i,close)
@@ -409,6 +589,7 @@ def run_simulation(stock_to_trade):
                 fig, ax = plt.subplots()
                 show_plt(real_index)
                 print(positions)
+            
             #############################################################
             #                     STRATEGY                              #
             #############################################################
