e3fdad8fdc24c7ce1ef5333fde1e68eb4d74376b
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2022-01-24 22:51:14 +0200

    Ready to write to second DB

--- Preprod/playground/on_db_updt.py
+++ Preprod/playground/on_db_updt.py
@@ -1,7 +1,10 @@
+from numpy import lib
 import psycopg2
 from datetime import datetime as dt
 import time
 import multiprocessing
+import talib
+import numpy as np
 
 def get_from_db(symbol):
     
@@ -25,16 +28,38 @@ def get_from_db(symbol):
             now = dt.now().minute
             
             if time_obj != last_update:
-                cur.execute("SELECT close FROM dbo.init_ohlc where symbol = '"+symbol+"' ORDER BY TIMESTAMP DESC LIMIT 15")
+                cur.execute("SELECT open,close,high,low FROM dbo.init_ohlc where symbol = '"+symbol+"' ORDER BY TIMESTAMP DESC LIMIT 15")
                 rows = cur.fetchall()
                 for r in rows:
-                    close = r[0]
-                    closes.append(close)
+                    close = r[1]
+                    closes.append(float(close))
                 print(f"closes for {symbol}")
                 print(closes)
                 last_update = time_obj
                 
-                closes = []
+                
+                closes = np.array(closes)
+
+                
+                roc_thin = talib.ROCP(closes, timeperiod = 5)
+                
+                roc_sma_5 = talib.SMA(roc_thin, timeperiod = 5)
+
+                roc_sma_15 = talib.SMA(roc_thin, timeperiod = 15)
+                
+                rsi = talib.RSI(closes ,timeperiod=14)
+                
+                #these values will be put in to a sperate table
+                roc_delta_15 = roc_sma_15[-1] - roc_sma_15[-2]
+                f_roc_sma_5  = roc_sma_5[-1]
+                f_roc_sma_15 = roc_sma_15[-1]
+                final_rsi    = rsi[-1]
+                
+                ocpen = rows[0][0]
+                close = rows[0][1]
+                high  = rows[0][2]
+                low   = rows[0][3]
+                
                     
             else:
                 print(f'not recent for {symbol}')
