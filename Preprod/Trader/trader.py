44efcccbf9a028db3b90e1266c4aeeedf81ba4f5
ariel <ariel.tar@99gmail.com>
2022-03-04 10:25:20 -0500

    api keys loaded

--- Preprod/Trader/trader.py
+++ Preprod/Trader/trader.py
@@ -86,8 +86,8 @@ def main(i):
     global worker_num,api
 
     ########## account info ############################
-    API_ID = 'PKDFI1CW15B78WND3AJ1'
-    API_KEY = 'TpzUdqKreikuV8g7MGI5bZrd3TQkSxX8viwfaXo9'
+    API_ID = 'PKT1ZJ5DBXFDTAQFXKGO'
+    API_KEY = 'M6m7qV2XTd6zjgoQpMMtrzhANAJXN6SBB053n4nj'
     api_endpoint = 'https://paper-api.alpaca.markets'
     ####################################################
     api = tradeapi.REST(key_id = API_ID,secret_key = API_KEY,base_url = api_endpoint)
